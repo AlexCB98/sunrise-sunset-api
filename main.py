15aba993f874dbeac7cc75b3348571c1f41c455d
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-06-25 11:27:20 +0200

    Finish Sunrise & Sunset API project

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import requests
 
-LAT = 48.669102
-LNG = 12.690720
 
-response = requests.get(url='https://api.sunrise-sunset.org/json')
+# https://www.latlong.net/
+LAT = 48.669102
+LONG = 12.690720
+
+parameters = {
+    'lat':LAT,
+    'lng':LONG,
+    'formatted':0
+}
+
+response = requests.get(
+    url='https://api.sunrise-sunset.org/json',
+    params=parameters,
+)
 response.raise_for_status()
 
 data = response.json()
 
-parameters = {
-    'lat':LAT,
-    'lng':LNG
-}
+sunrise = data['results']['sunrise'].split('T')[1].split(':')[0]
+sunset  = data['results']['sunset'].split('T')[1].split(':')[0]
 
+print(f'The sunrise will be at: {sunrise}')
+print(f'The sunset will be at: {sunset}')
